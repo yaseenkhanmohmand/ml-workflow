4247ec05a1dcdbda36ed99397306ef28377566db
Shivas Jayaram <shivasj@gmail.com>
2023-10-19 06:39:36 -0400

    Added model_deploy component

--- src/workflow/model.py
+++ src/workflow/model.py
@@ -53,3 +53,44 @@ def model_training(
         base_output_dir=MODEL_DIR,
         sync=True,
     )
+
+
+# Define a Container Component
+@dsl.component(
+    base_image="python:3.10", packages_to_install=["google-cloud-aiplatform"]
+)
+def model_deploy(
+    GCS_BUCKET_NAME: str = "",
+):
+    print("Model Training Job")
+
+    import google.cloud.aiplatform as aip
+
+    # List of prebuilt containers for prediction
+    # https://cloud.google.com/vertex-ai/docs/predictions/pre-built-containers
+    serving_container_image_uri = (
+        "us-docker.pkg.dev/vertex-ai/prediction/tf2-cpu.2-12:latest"
+    )
+
+    display_name = "Mushroom App Model"
+    ARTIFACT_URI = f"gs://{GCS_BUCKET_NAME}/model"
+
+    # Upload and Deploy model to Vertex AI
+    # Reference: https://cloud.google.com/python/docs/reference/aiplatform/latest/google.cloud.aiplatform.Model#google_cloud_aiplatform_Model_upload
+    deployed_model = aip.Model.upload(
+        display_name=display_name,
+        artifact_uri=ARTIFACT_URI,
+        serving_container_image_uri=serving_container_image_uri,
+    )
+    print("deployed_model:", deployed_model)
+    # Reference: https://cloud.google.com/python/docs/reference/aiplatform/latest/google.cloud.aiplatform.Model#google_cloud_aiplatform_Model_deploy
+    endpoint = deployed_model.deploy(
+        deployed_model_display_name=display_name,
+        traffic_split={"0": 100},
+        machine_type="n1-standard-4",
+        accelerator_count=0,
+        min_replica_count=1,
+        max_replica_count=1,
+        sync=True,
+    )
+    print("endpoint:", endpoint)
