181a3916b6be56f7b40d9a47c6160c1673fb2802
Shivas Jayaram <shivasj@gmail.com>
2023-10-19 05:56:01 -0400

    model training

--- src/workflow/docker-shell.sh
+++ src/workflow/docker-shell.sh
@@ -8,6 +8,8 @@ export SECRETS_DIR=$(pwd)/../../../secrets/
 export GCP_PROJECT="ac215-project"
 export GCS_BUCKET_NAME="mushroom-app-ml-workflow-demo"
 export GCS_SERVICE_ACCOUNT="ml-workflow@ac215-project.iam.gserviceaccount.com"
+export GCP_REGION="us-central1"
+export GCS_PACKAGE_URI="gs://mushroom-app-trainer-code"
 
 # Build the image based on the Dockerfile
 #docker build -t $IMAGE_NAME -f Dockerfile .
@@ -25,5 +27,7 @@ docker run --rm --name $IMAGE_NAME -ti \
 -e GCP_PROJECT=$GCP_PROJECT \
 -e GCS_BUCKET_NAME=$GCS_BUCKET_NAME \
 -e GCS_SERVICE_ACCOUNT=$GCS_SERVICE_ACCOUNT \
+-e GCP_REGION=$GCP_REGION \
+-e GCS_PACKAGE_URI=$GCS_PACKAGE_URI \
 $IMAGE_NAME
 
